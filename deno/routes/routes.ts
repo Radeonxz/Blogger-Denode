0b4c85b3bc3e917c7823d8cbd25833dae2d10c4a
Radeonxz <xuanxxzhao@gmail.com>
2022-03-04 18:47:54 -0500

    chore: remove ejs

--- deno/routes/routes.ts
+++ deno/routes/routes.ts
@@ -1,19 +1,19 @@
 import { Router } from "https://deno.land/x/oak@v10.4.0/mod.ts";
 
-import articlesRouter from "./articles/articlesRoutes.ts";
+// import articlesRouter from "./articles/articlesRoutes.ts";
 import ArticleModel from "../models/articles/ArticleModel.ts";
 
 const appRouter = new Router();
 
-appRouter
-  .get("/", async (ctx: any) => {
-    try {
-      const articles = await ArticleModel.find().toArray();
-      ctx.render("home/home.ejs", { articles });
-    } catch (error) {
-      console.log(error);
-    }
-  })
-  .use("/articles", articlesRouter.routes(), articlesRouter.allowedMethods());
+appRouter.get("/", async (ctx: any) => {
+  try {
+    const articles = await ArticleModel.find().toArray();
+    // ctx.render("home/home.ejs", { articles });
+    ctx.response.body = articles;
+  } catch (error) {
+    console.log(error);
+  }
+});
+// .use("/articles", articlesRouter.routes(), articlesRouter.allowedMethods());
 
 export default appRouter;
--- deno/server.ts
+++ deno/server.ts
@@ -1,25 +1,25 @@
 import { Application } from "https://deno.land/x/oak@v10.4.0/mod.ts";
-import {
-  viewEngine,
-  engineFactory,
-  adapterFactory,
-  ViewConfig
-} from "https://deno.land/x/view_engine@v1.5.0/mod.ts";
+// import {
+//   viewEngine,
+//   engineFactory,
+//   adapterFactory,
+//   ViewConfig
+// } from "https://deno.land/x/view_engine@v1.5.0/mod.ts";
 import staticFiles from "https://deno.land/x/static_files@1.1.6/mod.ts";
 
 import appRouter from "./routes/routes.ts";
 
 // View engine setup
-const ejsEngine = engineFactory.getEjsEngine();
-const oakAdapter = adapterFactory.getOakAdapter();
+// const ejsEngine = engineFactory.getEjsEngine();
+// const oakAdapter = adapterFactory.getOakAdapter();
 
 // Init app server
 const app = new Application();
 
-const viewConfig: ViewConfig = {
-  viewRoot: "views"
-};
-app.use(viewEngine(oakAdapter, ejsEngine, viewConfig));
+// const viewConfig: ViewConfig = {
+//   viewRoot: "views"
+// };
+// app.use(viewEngine(oakAdapter, ejsEngine, viewConfig));
 
 // Routes and methods setup
 app.use(appRouter.routes());
